0a74bba1c72a4e1b526af4889939bd3a65cd70ac
ffeaugas <feaugas.francis@gmail.com>
2023-10-08 15:22:20 -0400

    Putting Next, Nest and MySQL in separated dockers - Still need to add Next to the docker-compose but ITS WORKING ! Prisma unliked to SQLite and now linked to MySQL

--- server/prisma/migrations/20230910091438_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updateAt" DATETIME NOT NULL,
-    "name" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL DEFAULT false
-);
