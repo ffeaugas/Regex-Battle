ae4c18a3ab09bfa519f9b29b4f15e48da5ea8b35
ffeaugas <feaugas.francis@gmail.com>
2023-10-11 11:23:38 -0400

    Started writting the level.service tests !

--- server/src/level/level.service.spec.ts
+++ server/src/level/level.service.spec.ts
@@ -1,19 +1,68 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { LevelService } from "./level.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { Level } from "@prisma/client";
+
+const dateMock: Date = new Date();
+
+const levelArray: Level[] = [
+  {
+    id: 1,
+    createdAt: dateMock,
+    updatedAt: dateMock,
+    title: "TestLevel1",
+    type: "Match",
+    statement: "Do smthing",
+    input: "A sentence with key",
+    output: "key",
+    solution: "key",
+  },
+  {
+    id: 2,
+    createdAt: dateMock,
+    updatedAt: dateMock,
+    title: "TestLevel2",
+    type: "Match",
+    statement: "Do smthingggg",
+    input: "A sentence with ku",
+    output: "ku",
+    solution: "ku",
+  },
+];
+
+const prismaMock = {
+  level: {
+    findMany: jest.fn().mockResolvedValue(levelArray),
+  },
+};
 
 describe("LevelService", () => {
   let service: LevelService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LevelService, PrismaService],
+      providers: [
+        LevelService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<LevelService>(LevelService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("getAll", () => {
+    it("should return an array of levels", async () => {
+      const levels = await service.getAll();
+      expect(levels).toEqual(levelArray);
+    });
   });
 });
